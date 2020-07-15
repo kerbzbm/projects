955061739078d42222fb534b29fafd6da8e80632
kerbzbm <mitchdouble@gmail.com>
2020-07-15 11:20:11 -0300

    Added DOM Model picture and selector intro files.

---
+++ DOM_Learning/selectorsIntro.js
@@ -0,0 +1,20 @@
+// Selects all elements that have an ID that matches the argument.
+document.getElementById("highlight"); {
+ 
+}
+// Selects all elements that have a class name that matches the argument returns a list (HTMLCollection).
+document.getElementsByClassName("bolded"); {
+
+}
+// Selects all elements that have a tag that matches the argument and returns a list (HTMLCollection).
+document.getElementsByTagName("li"); {
+
+}
+// Returns the first element with a CSS selector that matches the argument.
+document.querySelector("#highlight"); {
+
+}
+// Returns all elements with a CSS selector that matches the argument.
+document.querySelectorAll("#highlight"); {
+
+}
